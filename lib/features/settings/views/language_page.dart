25e4117dc27c5e4bbd4060420482424e8ce34168
Thiago Windsurf <contato.dev.thiagosilva@gmail.com>
2025-02-28 11:32:55 -0300

    refactor: move CustomSearchBar and LoadingPage to shared components

--- lib/features/settings/views/language_page.dart
+++ lib/features/settings/views/language_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:windsurf_test/core/components/custom_search_bar.dart';
 import 'package:windsurf_test/core/utils/constants/app_strings.dart';
 import 'package:windsurf_test/core/providers/language_provider.dart';
+
+import '../../../core/shared/widget/custom_search_bar.dart';
 
 class LanguagePage extends ConsumerStatefulWidget {
   const LanguagePage({super.key});
